fc0c8727bd1ac9d959dd0bb37b7071776f5e1273
Kevin_F <kevinfolvert@gmail.com>
2023-03-28 11:36:26 -0400

    US 9 version 1

--- src/main/java/dao/TestHibernate.java
+++ src/main/java/dao/TestHibernate.java
@@ -38,10 +38,10 @@ public class TestHibernate
 	 */
 	/*----- Création et enregistrement d'employés -----*/
 	public static void createCours ()
-		{
+	{
 		/*----- Ouverture de la session -----*/
 		try (Session session = HibernateUtil.getSessionFactory().getCurrentSession())
-			{
+		{
 			/*----- Ouverture d'une transaction -----*/
 			Transaction t = session.beginTransaction();
 
@@ -69,231 +69,234 @@ public class TestHibernate
 
 			Users u1 =  session.get(Users.class, 1);
 			Users u2 =  session.get(Users.class, 2);
-			
+
 			u1.ajouteCours(c6);
 			u2.ajouteCours(c1);
-			
+
 			t.commit();
 			session.close();
-			}
 		}
+	}
 
 	public static void createUsers () throws ParseException
 	{
-	/*----- Ouverture de la session -----*/
-	try (Session session = HibernateUtil.getSessionFactory().getCurrentSession())
+		/*----- Ouverture de la session -----*/
+		try (Session session = HibernateUtil.getSessionFactory().getCurrentSession())
 		{
-		/*----- Ouverture d'une transaction -----*/
-		Transaction t = session.beginTransaction();
+			/*----- Ouverture d'une transaction -----*/
+			Transaction t = session.beginTransaction();
 
-		Enseignant u1=new Enseignant("weichen@gmail.com","1234","sun","weichen","Mme",DFDATE.parse("15/11/1998"),"aaa@qqq","aaaaaaaaaa","MF201");
-		Enseignant u2=new Enseignant("zhibo@gmail.com","1234","xie","zhibo","M",DFDATE.parse("04/04/1998"),"aaa@qqq","aaaaaaaaaa","MF202");
+			Enseignant u1=new Enseignant("weichen@gmail.com","1234","sun","weichen","Mme",DFDATE.parse("15/11/1998"),"aaa@qqq","aaaaaaaaaa","MF201");
+			Enseignant u2=new Enseignant("zhibo@gmail.com","1234","xie","zhibo","M",DFDATE.parse("04/04/1998"),"aaa@qqq","aaaaaaaaaa","MF202");
 
-		Etudiant u3=new Etudiant("marong@gmail.com","1234","Ma","rong","Mme",DFDATE.parse("15/11/1998"),"aaa@qqq","aaaaaaaaaa","IPM","FI");
-		Etudiant u4=new Etudiant("jean@gmail.com","1234","Jean","martin","M",DFDATE.parse("04/04/1998"),"aaa@qqq","aaaaaaaaaa","IPM","FA");
-		Etudiant u5=new Etudiant("thomas@gmail.com","1234","Thomas","Robert","M",DFDATE.parse("15/11/1998"),"aaa@qqq","aaaaaaaaaa","IPM","FI");
-		Etudiant u6=new Etudiant("girard@gmail.com","1234","Girard","Marty","M",DFDATE.parse("04/04/1998"),"aaa@qqq","aaaaaaaaaa","IPM","FA");
-		Etudiant u7=new Etudiant("vincent@gmail.com","1234","Vincent","Julien","M",DFDATE.parse("15/11/1998"),"aaa@qqq","aaaaaaaaaa","IPM","FI");
-		Etudiant u8=new Etudiant("benoit@gmail.com","1234","Benoit","Jacob","M",DFDATE.parse("04/04/1998"),"aaa@qqq","aaaaaaaaaa","IPM","FA");
+			Etudiant u3=new Etudiant("marong@gmail.com","1234","Ma","rong","Mme",DFDATE.parse("15/11/1998"),"aaa@qqq","aaaaaaaaaa","IPM","FI");
+			Etudiant u4=new Etudiant("jean@gmail.com","1234","Jean","martin","M",DFDATE.parse("04/04/1998"),"aaa@qqq","aaaaaaaaaa","IPM","FA");
+			Etudiant u5=new Etudiant("thomas@gmail.com","1234","Thomas","Robert","M",DFDATE.parse("15/11/1998"),"aaa@qqq","aaaaaaaaaa","IPM","FI");
+			Etudiant u6=new Etudiant("girard@gmail.com","1234","Girard","Marty","M",DFDATE.parse("04/04/1998"),"aaa@qqq","aaaaaaaaaa","IPM","FA");
+			Etudiant u7=new Etudiant("vincent@gmail.com","1234","Vincent","Julien","M",DFDATE.parse("15/11/1998"),"aaa@qqq","aaaaaaaaaa","IPM","FI");
+			Etudiant u8=new Etudiant("benoit@gmail.com","1234","Benoit","Jacob","M",DFDATE.parse("04/04/1998"),"aaa@qqq","aaaaaaaaaa","IPM","FA");
 
-		Scolarite u9=new Scolarite("robin@gmail.com","1234","Robin","Adam","Mme",DFDATE.parse("15/11/1998"),"aaa@qqq","aaaaaaaaaa","ME001");
-		Scolarite u10=new Scolarite("barbier@gmail.com","1234","Barbier","Antoine","M",DFDATE.parse("04/04/1998"),"aaa@qqq","aaaaaaaaaa","MF204");
+			Scolarite u9=new Scolarite("robin@gmail.com","1234","Robin","Adam","Mme",DFDATE.parse("15/11/1998"),"aaa@qqq","aaaaaaaaaa","ME001");
+			Scolarite u10=new Scolarite("barbier@gmail.com","1234","Barbier","Antoine","M",DFDATE.parse("04/04/1998"),"aaa@qqq","aaaaaaaaaa","MF204");
 
-		session.save(u1);
-		session.save(u2);
-		session.save(u3);
-		session.save(u4);
-		session.save(u5);
-		session.save(u6);
-		session.save(u7);
-		session.save(u8);
-		session.save(u9);
-		session.save(u10);
-	
-		t.commit();
-		session.close();
+			session.save(u1);
+			session.save(u2);
+			session.save(u3);
+			session.save(u4);
+			session.save(u5);
+			session.save(u6);
+			session.save(u7);
+			session.save(u8);
+			session.save(u9);
+			session.save(u10);
+
+			t.commit();
+			session.close();
 		}
 	}
 	public static void createSeance () throws ParseException
 	{
-	/*----- Ouverture de la session -----*/
-	try (Session session = HibernateUtil.getSessionFactory().getCurrentSession())
+		/*----- Ouverture de la session -----*/
+		try (Session session = HibernateUtil.getSessionFactory().getCurrentSession())
 		{
-		/*----- Ouverture d'une transaction -----*/
-		Transaction t = session.beginTransaction();
-		
-		Users u1 =  session.get(Users.class, 1);
-		Cours c1 =  session.get(Cours.class, 1);
-		Cours c2 =  session.get(Cours.class, 2);
-		Cours c3 =  session.get(Cours.class, 3);
-		Cours c4 =  session.get(Cours.class, 4);
-		Cours c10 =  session.get(Cours.class, 10);
-		
-		Seance s1= new Seance("ME401",DFDATE.parse("28/03/2023"),270,DF.parse("28/03/2023 08:00"),"valide",u1,c4);
-		Seance s2= new Seance("MF105",DFDATE.parse("27/03/2023"),180,DF.parse("27/03/2023 14:00"),"enregistrer",u1,c3);
-		Seance s3= new Seance("ME310",DFDATE.parse("28/03/2023"),180,DF.parse("28/03/2023 14:00"),"novalide",u1,c10);
-		
-		Seance s4= new Seance("ME410",DFDATE.parse("29/03/2023"),90,DF.parse("29/03/2023 11:00"),"novalide",u1,c3);
-		Seance s5= new Seance("MF103",DFDATE.parse("29/03/2023"),180,DF.parse("29/03/2023 14:00"),"novalide",u1,c3);
-		Seance s6= new Seance("MC405",DFDATE.parse("30/03/2023"),180,DF.parse("30/03/2023 09:30"),"novalide",u1,c1);
+			/*----- Ouverture d'une transaction -----*/
+			Transaction t = session.beginTransaction();
 
-		Seance s7= new Seance("ME403",DFDATE.parse("31/03/2023"),180,DF.parse("31/03/2023 15:30"),"novalide",u1,c4);
-		
-		session.save(s1);
-		session.save(s2);
-		session.save(s3);
-		session.save(s4);
-		session.save(s5);
-		session.save(s6);
-		session.save(s7);
+			Users u1 =  session.get(Users.class, 1);
+			Cours c1 =  session.get(Cours.class, 1);
+			Cours c2 =  session.get(Cours.class, 2);
+			Cours c3 =  session.get(Cours.class, 3);
+			Cours c4 =  session.get(Cours.class, 4);
+			Cours c10 =  session.get(Cours.class, 10);
 
-		
-		t.commit();
-		session.close();
-		
+			Seance s1= new Seance("ME401",DFDATE.parse("28/03/2023"),270,DF.parse("28/03/2023 08:00"),"valide",u1,c4);
+			Seance s2= new Seance("MF105",DFDATE.parse("27/03/2023"),180,DF.parse("27/03/2023 14:00"),"enregistrer",u1,c3);
+			Seance s3= new Seance("ME310",DFDATE.parse("28/03/2023"),180,DF.parse("28/03/2023 14:00"),"novalide",u1,c10);
+
+			Seance s4= new Seance("ME410",DFDATE.parse("29/03/2023"),90,DF.parse("29/03/2023 11:00"),"novalide",u1,c3);
+			Seance s5= new Seance("MF103",DFDATE.parse("29/03/2023"),180,DF.parse("29/03/2023 14:00"),"novalide",u1,c3);
+			Seance s6= new Seance("MC405",DFDATE.parse("30/03/2023"),180,DF.parse("30/03/2023 09:30"),"novalide",u1,c1);
+
+			Seance s7= new Seance("ME403",DFDATE.parse("31/03/2023"),180,DF.parse("31/03/2023 15:30"),"novalide",u1,c4);
+
+			session.save(s1);
+			session.save(s2);
+			session.save(s3);
+			session.save(s4);
+			session.save(s5);
+			session.save(s6);
+			session.save(s7);
+
+
+			t.commit();
+			session.close();
+
 		}
-	
+
 	}
 	public static void createParticipe () throws ParseException
 	{
-	/*----- Ouverture de la session -----*/
-	try (Session session = HibernateUtil.getSessionFactory().getCurrentSession())
+		/*----- Ouverture de la session -----*/
+		try (Session session = HibernateUtil.getSessionFactory().getCurrentSession())
 		{
-		/*----- Ouverture d'une transaction -----*/
-		Transaction t = session.beginTransaction();
-		
-		Users u3 =  session.get(Users.class, 3);
-		Users u4 =  session.get(Users.class, 4);
-		Users u5 =  session.get(Users.class, 5);
-		Users u6 =  session.get(Users.class, 6);
-		Users u7 =  session.get(Users.class, 7);
-		Users u8 =  session.get(Users.class, 8);
+			/*----- Ouverture d'une transaction -----*/
+			Transaction t = session.beginTransaction();
 
-		
-		Seance s1= session.get(Seance.class, 1);
-		Seance s2= session.get(Seance.class, 2);
-		Seance s3= session.get(Seance.class, 3);
-		Seance s4= session.get(Seance.class, 4);
-		Seance s5= session.get(Seance.class, 5);
-		Seance s6= session.get(Seance.class, 6);
-		Seance s7= session.get(Seance.class, 7);
+			Users u3 =  session.get(Users.class, 3);
+			Users u4 =  session.get(Users.class, 4);
+			Users u5 =  session.get(Users.class, 5);
+			Users u6 =  session.get(Users.class, 6);
+			Users u7 =  session.get(Users.class, 7);
+			Users u8 =  session.get(Users.class, 8);
 
-		
-		u3.participe(s1, "present");
-		u4.participe(s1, "present");
-		u5.participe(s1, "present");
-		u6.participe(s1, "present");
-		u7.participe(s1, "present");
-		u8.participe(s1, "present");
-		
-		u3.participe(s2, "present");
-		u4.participe(s2, "present");
-		u5.participe(s2, "present");
-		u6.participe(s2, "present");
-		u7.participe(s2, "present");
-		u8.participe(s2, "present");
-		
-		u3.participe(s3, "present");
-		u4.participe(s3, "present");
-		u5.participe(s3, "present");
-		u6.participe(s3, "present");
-		u7.participe(s3, "present");
-		u8.participe(s3, "present");
-		
-		u3.participe(s4, "present");
-		u4.participe(s4, "present");
-		u5.participe(s4, "present");
-		u6.participe(s4, "present");
-		u7.participe(s4, "present");
-		u8.participe(s4, "present");
-		
-		u3.participe(s5, "present");
-		u4.participe(s5, "present");
-		u5.participe(s5, "present");
-		u6.participe(s5, "present");
-		u7.participe(s5, "present");
-		u8.participe(s5, "present");
-		
-		u3.participe(s6, "present");
-		u4.participe(s6, "present");
-		u5.participe(s6, "present");
-		u6.participe(s6, "present");
-		u7.participe(s6, "present");
-		u8.participe(s6, "present");
-		
-		u3.participe(s7, "present");
-		u4.participe(s7, "present");
-		u5.participe(s7, "present");
-		u6.participe(s7, "present");
-		u7.participe(s7, "present");
-		u8.participe(s7, "present");
-		
-		t.commit();
-		session.close();
-		
+
+			Seance s1= session.get(Seance.class, 1);
+			Seance s2= session.get(Seance.class, 2);
+			Seance s3= session.get(Seance.class, 3);
+			Seance s4= session.get(Seance.class, 4);
+			Seance s5= session.get(Seance.class, 5);
+			Seance s6= session.get(Seance.class, 6);
+			Seance s7= session.get(Seance.class, 7);
+
+
+			u3.participe(s1, "present");
+			u4.participe(s1, "present");
+			u5.participe(s1, "present");
+			u6.participe(s1, "present");
+			u7.participe(s1, "present");
+			u8.participe(s1, "present");
+
+			u3.participe(s2, "present");
+			u4.participe(s2, "present");
+			u5.participe(s2, "present");
+			u6.participe(s2, "present");
+			u7.participe(s2, "present");
+			u8.participe(s2, "present");
+
+			u3.participe(s3, "present");
+			u4.participe(s3, "present");
+			u5.participe(s3, "present");
+			u6.participe(s3, "present");
+			u7.participe(s3, "present");
+			u8.participe(s3, "present");
+
+			u3.participe(s4, "present");
+			u4.participe(s4, "present");
+			u5.participe(s4, "present");
+			u6.participe(s4, "present");
+			u7.participe(s4, "present");
+			u8.participe(s4, "present");
+
+			u3.participe(s5, "present");
+			u4.participe(s5, "present");
+			u5.participe(s5, "present");
+			u6.participe(s5, "present");
+			u7.participe(s5, "present");
+			u8.participe(s5, "present");
+
+			u3.participe(s6, "present");
+			u4.participe(s6, "present");
+			u5.participe(s6, "present");
+			u6.participe(s6, "present");
+			u7.participe(s6, "present");
+			u8.participe(s6, "present");
+
+			u3.participe(s7, "present");
+			u4.participe(s7, "present");
+			u5.participe(s7, "present");
+			u6.participe(s7, "present");
+			u7.participe(s7, "present");
+			u8.participe(s7, "present");
+
+			t.commit();
+			session.close();
+
 		}
-	
+
 	}
 	public static void createDeposerJus () throws ParseException
 	{
-	/*----- Ouverture de la session -----*/
-	try (Session session = HibernateUtil.getSessionFactory().getCurrentSession())
+		/*----- Ouverture de la session -----*/
+		try (Session session = HibernateUtil.getSessionFactory().getCurrentSession())
 		{
-		/*----- Ouverture d'une transaction -----*/
-		Transaction t = session.beginTransaction();
-		
-		Users u3 =  session.get(Users.class, 1);
-		Users u4 = session.get(Etudiant.class, 4);
-	
+			/*----- Ouverture d'une transaction -----*/
+			Transaction t = session.beginTransaction();
 
-		//Justificatif j1=new Justificatif(false,"www",DFDATE.parse("04/01/2023"),DFDATE.parse("05/01/2023"),u3);
-		Justificatif j2 = new Justificatif(false,"http", DFDATE.parse("05/01/2023"),DFDATE.parse("06/01/2023"),u4);
+			Users u3 =  session.get(Users.class, 1);
+			Users u4 = session.get(Etudiant.class, 4);
 
-		u4.getJustificatifs().add(j2);
-		session.save(j2);
-		
-		t.commit();
-		session.close();
-		
+
+			Justificatif j1=new Justificatif(false,"www",DFDATE.parse("04/01/2023"),DFDATE.parse("05/01/2023"),u3);
+			Justificatif j2 = new Justificatif(false,"http", DFDATE.parse("05/01/2023"),DFDATE.parse("06/01/2023"),u4);
+
+			u3.getJustificatifs().add(j1);
+			u4.getJustificatifs().add(j2);
+			session.save(j2);
+
+			t.commit();
+			session.close();
+
 		}
-	
+
 	}
 
 	public static List<Seance> loadSeancesDonner (int id) 
 	{
-	/*----- Ouverture de la session -----*/
-	try (Session session = HibernateUtil.getSessionFactory().getCurrentSession())
+		/*----- Ouverture de la session -----*/
+		try (Session session = HibernateUtil.getSessionFactory().getCurrentSession())
 		{
-		/*----- Ouverture d'une transaction -----*/
-		Transaction t = session.beginTransaction();
-		
-    	List<Seance> seances = new ArrayList<>();
-    	
-    	Query query = session.createQuery("from Seance s where s.CodeU = :id");
-    	query.setParameter("id", id);
-        if (!query.getResultList().isEmpty()) {
-            	seances=query.list();
-            	query.list().forEach(System.out::println);
-            }
-        return seances;
+			/*----- Ouverture d'une transaction -----*/
+			Transaction t = session.beginTransaction();
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-	return null;
-        
+			List<Seance> seances = new ArrayList<>();
+
+			Query query = session.createQuery("from Seance s where s.CodeU = :id");
+			query.setParameter("id", id);
+			if (!query.getResultList().isEmpty()) {
+				seances=query.list();
+				query.list().forEach(System.out::println);
+			}
+			return seances;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+
 	}
 
+	
 	/**
-	 * Recupère et affiche les etudiants qui n'ont pas justifier leur presence
+	 * Recupère et affiche les justificatifs non valide
+	 * Penser a changer si on rajoute des ISIAD
 	 * @return 
 	 */
-	public static List<Etudiant> loadEtuAbsNonJustifier() {
+	public static List<Justificatif> loadJustificatifNonValide() {
 		try(Session session = HibernateUtil.getSessionFactory().getCurrentSession()) {
 			Transaction t = session.beginTransaction();
 
 			//Liste des etudiants absence non justifier
-			String  hql = "SELECT e FROM Etudiant e JOIN e.justificatifs j WHERE j.validation = 0" ;		//Requete pour recupérer les étudiants
+			String  hql = "SELECT j FROM Justificatif j JOIN j.users u WHERE j.validation = 0 AND u.parcours = 'IPM' " ;		//Requete pour recupérer les étudiants
 
 
 			List queryResponse = session.createQuery(hql).list();
@@ -303,35 +306,54 @@ public class TestHibernate
 
 			t.commit();
 			return queryResponse;
-			
+
 		}
-	
+	}
+
+	public static void updateJustificatif(int justificatifId, boolean validation) {
+		/*----- Ouverture de la session -----*/
+		try (Session session = HibernateUtil.getSessionFactory().getCurrentSession())
+		{
+			/*----- Ouverture d'une transaction -----*/
+			Transaction t = session.beginTransaction(); 
+
+			
+			Justificatif j1 = session.get(Justificatif.class, justificatifId);	//Recup�re le justificatif
+			
+			j1.setValidation(validation);	//change sa validation
+			session.update(j1);				//Met � jour
+			
+			t.commit();
+			session.close();
+		}
 	}
 	/**
 	 * Programme de test.
 	 */
 	public static void main(String[] args) throws ParseException
-		{
+	{
+
+		//		TestHibernate.createUsers();
+		//		TestHibernate.createCours();
+		//		TestHibernate.createSeance();
+		//		TestHibernate.createParticipe();
+		//		TestHibernate.createDeposerJus();
+		//TestHibernate.updateJustificatif(1, false);
 		
-//		TestHibernate.createUsers();
-//		TestHibernate.createCours();
-//		TestHibernate.createSeance();
-//		TestHibernate.createParticipe();
-//		TestHibernate.createDeposerJus();
-	
-		TestHibernate.loadEtuAbsNonJustifier();
+		//TestHibernate.loadJustificatifNonValide();
+
 		//TestHibernate.loadSeancesDonner(1);
-		}
+	}
 
 	private static void affichage (List l)
 	{
-	System.out.println("-----");
-	l.forEach(e -> {
-		for (Object obj : (Object[])e)
-			System.out.print(obj + " ");
-		System.out.println();
-	});
-	System.out.println("-----");
+		System.out.println("-----");
+		l.forEach(e -> {
+			for (Object obj : (Object[])e)
+				System.out.print(obj + " ");
+			System.out.println();
+		});
+		System.out.println("-----");
 	}
 
 	/**Lecture d'une liste de tableau d'objets
@@ -348,18 +370,18 @@ public class TestHibernate
 	}
 
 	/**
-	 * Lecture cast des etudiants
+	 * Lecture cast des justificatitfs
 	 * @param l
 	 */
 	public static void lire1(List l) {
 		for (Object obj : l) {
-			if (obj instanceof Etudiant) {
-				Etudiant etudiant = (Etudiant) obj;
+			if (obj instanceof Justificatif) {
+				Justificatif etudiant = (Justificatif) obj;
 				System.out.println(etudiant);
 			}
 		}
 	}
 
-	
+
 } /*----- Fin de la classe TestHibernate -----*/
 
--- src/main/webapp/Scolarite/VerifJustificatif.jsp
+++ src/main/webapp/Scolarite/VerifJustificatif.jsp
@@ -7,44 +7,109 @@
 <!DOCTYPE html>
 <html>
 <head>
+
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>Etudiant Absent</title>
+<link rel="stylesheet" type="text/css" href="css/VerifJustificatif.css">
+
+<style>
+.invisible {
+	display: none;
+}
+</style>
+
+
 </head>
 <body>
 
-	<h1>
-		Etudiants de 
-		<%= request.getParameter("type_promo") %></h1>
+	<!-- menu -->
+	<div class="menu">
+		<ul>
+			<li><img src="img/logo-ut1.png" class="logo-ut1" /></li>
+			<div class="accueil">
+				<li><a href="Scolarite/AcceuilScolarite.jsp">Accueil</a>
+			</div>
+			</li>
 
-	<table border="1">
-		<thead>
-			<tr>
-				<th>Nom</th>
-				<th>Prénom</th>
-				<th>Motif d'absence</th>
-				<th>Valider</th>
-			</tr>
-		</thead>
-		<tbody>
-			<%
-			List<Object> listeAbs = (List<Object>) request.getAttribute("listeAbs");
-				            for (Object obj : listeAbs) {
-				                    if (obj instanceof Users) {
-				                    	 Users etudiant = (Users) obj;
-				                    	
-				//                 for (Utilisateurs etudiant : (ArrayList<Utilisateurs>) request.getAttribute("listeAbs"))
-			%>
-			<tr>
-				<td><%= etudiant.getNom() %></td>
-				<td><%= etudiant.getPrenom() %></td>
-				<td>
-					<%-- <%= etudiant.getJustificatif().getMotif() %> --%>
-				</td>
-				<td><button>Valider</button></td>
-			</tr>
-			<% } }%>
-		</tbody>
-	</table>
+			<div class="compte">
+				<li><a href="#">Mon Compte</a>
+					<ul>
+						<li><a href="CtrlProfil">Mes informations</a></li>
+						<li><a href="webapp/index.jsp">Déconnextion</a></li>
+					</ul>
+			</div>
+			</li>
+		</ul>
+	</div>
 
+	<div class="container">
+		<h1>
+			Les étudiants
+			<%=request.getParameter("type_promo")%>
+		</h1>
+
+		<form action="CtrlUpdateJustificatif" method="GET">
+
+			<%!String idChecked(long id, String[] tab) {
+		if (tab != null) {
+			for (String s : tab) {
+				if (s.equals(id + ""))
+					return " checked";
+			}
+		} else {
+			return "";
+		}
+		return "";
+	}%>
+			<table border="1">
+				<thead>
+					<tr>
+						<th>Nom</th>
+						<th>Prénom</th>
+						<th>Motif d'absence</th>
+						<th>Valider</th>
+					</tr>
+				</thead>
+				<tbody>
+					<!-- Object obj : listeAbs -->
+					<%
+					String[] id_check = (String[]) session.getAttribute("idChecked");
+
+					List<Object> listeAbs = (List<Object>) request.getAttribute("listeAbs"); //Recupère la liste
+					int nbJustif = listeAbs.size(); //nombre des justificatifs
+					for (int i = 0; i < nbJustif; i++) {
+						if (listeAbs.get(i) instanceof Justificatif) { //Si cest une instance de justif avec on le cast
+							Justificatif justificatif = (Justificatif) listeAbs.get(i); //devient un attribut
+					%>
+					<td class="invisible" id="nbJ"><%=nbJustif%></td>
+					<tr>
+						<td><%=justificatif.getUsersJustificatif().getNom()%></td>
+						<td><%=justificatif.getUsersJustificatif().getPrenom()%></td>
+						<td><%=justificatif.getUrl()%></td>
+						<td>
+							<%
+							out.println("<input type='checkbox' name='justifId' value='" + justificatif.getCodeJust() + "' "
+									+ idChecked(justificatif.getCodeJust(), id_check) + " />");
+							%>
+						</td>
+					</tr>
+					<%
+					}
+					}
+					%>
+				</tbody>
+
+			</table>
+			<div class="btn_action">
+				<button>
+					<a href="/DAI_projet_new/centrale">Retour</a>
+				</button>
+				<input type="submit" value="Enregistrer" />
+			</div>
+		</form>
+		<div>${requestScope.msg_erreur}</div>
+	</div>
+
+	<script type="text/JavaScript" src="js/Fonction.js"></script>
 </body>
 </html>
